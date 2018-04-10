e19981638158c9a09572f94bfc3deb5129600a6a
JohannaCap <36660353+JohannaCap@users.noreply.github.com>
2018-04-10 11:24:30 +0200

    cours4

---
+++ cours4/script.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+	welcome('Hello');
+	$("img").addClass('image');
+	 
+
+});
+
+function welcome() {
+   $("#welcome").html('<h4>Bienvenue, <br> soyez pret à renouveler de nouveaux défis !</h4>');
+};
